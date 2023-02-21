6b4bf4d77c978e4bdf9d7b197cb0b5ac7641f830
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-02-21 19:28:36 +0500

    Add all the data according to requerment

--- data.sql
+++ data.sql
@@ -3,3 +3,19 @@
 INSERT INTO animals (name) VALUES ('Luna');
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animal(id, animalname, animal_date_of_birth,
+animal_escape_attempts, neutered, animal_weight_kg) VALUES (
+    1, 'Agumon', '2020-02-03', 0, FALSE, 10.23
+);
+INSERT INTO animal(id, animalname, animal_date_of_birth,
+animal_escape_attempts, neutered, animal_weight_kg) VALUES (
+    2,'Gabumon', '2018-11-15', 2, TRUE, 8 
+);
+INSERT INTO ANIMAL(id, animalname, animal_date_of_birth,
+animal_escape_attempts, neutered, animal_weight_kg) VALUES (
+    2,'Pikachu', '2021-01-07', 1, FALSE, 15.04 
+);
+INSERT INTO ANIMAL(id, animalname, animal_date_of_birth,
+animal_escape_attempts, neutered, animal_weight_kg) VALUES (
+    3,'Devimon', '2021-01-07', 1, FALSE, 15.04 
+);
--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name = 'Luna';
+select * from animal where animalname like '%mon';
+select * from animal where animal_date_of_birth between '2016-11-15' AND '2019-11-15';
+select * from animal where neutered < 3 ;
+select * from animal where animalname between 'Agumon' AND 'Pikachu';
+select * from animal where neutered like true;
+select * from animal where animalname not like 'Gabumon';
+select * from animal where animal_weight_kg between 10 AND 17;
