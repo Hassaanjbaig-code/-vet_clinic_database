9ebac557e1b2bfb205f7adb14b48ea29dc81a8ef
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-02-21 20:35:44 +0500

    Update according to the review suggustion

--- schema.sql
+++ schema.sql
@@ -1,9 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100),
-);
-
 CREATE TABLE ANIMAL(id INTEGER,
 animalname varchar(50),
 animal_date_of_birth DATE,
