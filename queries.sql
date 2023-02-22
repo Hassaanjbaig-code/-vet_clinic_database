1142af53409798f359e5f539f2fbc4b96adbbcdd
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-02-22 13:03:16 +0500

    Takin gavg, count, min, max avg

--- queries.sql
+++ queries.sql
@@ -29,3 +29,11 @@ rollback to my_snapshot;
 update animal set animal_weight_kg = animal_weight_kg * -1 where animal_weight_kg < 0;
 commit;
 
+
+select count(animal_escape_attempts) from animal where animal_escape_attempts > 0;
+select avg(animal_weight_kg) from animal;
+select count(animal_escape_attempts) from animal group by neutered;
+select min(animal_weight_kg) from animal;
+select max(animal_weight_kg) from animal;
+SELECT species, ROUND(AVG(animal_escape_attempts)) FROM animal
+  WHERE animal_date_of_birth >= '1990-01-01' AND animal_date_of_birth <= '2000-12-31' GROUP BY species;
