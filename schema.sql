ca06d867b6cef29f82f618180aea3ec68f1357be
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-02-22 23:31:04 +0500

    create a table and foreign key

--- schema.sql
+++ schema.sql
@@ -10,3 +10,33 @@ animal_weight_kg DECIMAL
 
 ALTER TABLE animal
 ADD COLUMN species varchar(50);
+
+-- Create a table owners and species
+
+create table owners(
+id int auto_increment primary key,
+fullname varchar(100),
+age integer
+);
+
+create table species(
+id int auto_increment primary key,
+fullname varchar(100)
+);
+
+-- Delete the column species
+
+alter table `animal` drop column species;
+
+-- Add column of species_id and owner_id 
+alter table animal add species_id int;
+
+alter table animal add owner_id int;
+
+-- Reference and create the foreign key
+alter table animal add foreign key (species_id) references species(id);
+
+alter table animal add foreign key (owner_id ) references owners(id);
+
+select * from species;
+select * from owners;
