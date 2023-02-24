45b606e7468d9b20cc4be8757c62fd481528c8b9
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-02-24 22:04:14 +0500

    Insert a data in table

--- data.sql
+++ data.sql
@@ -69,3 +69,21 @@ update animal set owner_id = 4 where animal.animalname like 'Charmander' or anim
 update animal set animal.owner_id = 5 where animal.animalname like 'Angemon' or animal.animalname like 'Boarmon';
 
 select * from animal;
+
+insert into visits(animal_id, vets_id, date_visit) values (1, 1, '2020-05-24'), (1, 3, '2020-07-22'),
+(2, 4, '2021-02-02'), (3, 2, '2020-01-05'), (3, 2, '2020-03-08'), (3, 2 ,'2020-05-14'), ( 4, 3, '2021-05-04'), ( 5, 4, '2021-02-24'),
+( 6, 2, '2019-12-21'), ( 6, 1, '2020-08-10'), ( 6, 2, '2021-04-07'), ( 7, 2, '2019-09-29'), ( 1, 4, '2020-10-03'),
+( 1, 4, '2020-11-04'), ( 9, 2, '2019-01-24'), ( 9, 2, '2019-05-15'), ( 9, 2, '2020-02-27'), ( 9, 2, '2020-09-03'),
+( 10, 3, '2020-05-24'), ( 10, 1, '2021-01-11');
+
+select * from visits;
+
+insert into specializations(species_id, vets_id) values (1,1), (2,3),(1,3),(2,4);
+
+select * from specializations;
+
+
+insert into vets(name, age, date_of_graduation) values ('Vet William Tatcher', 45, '2000-04-23'), ('Vet Maisy Smith', 26, '2019-01-17')
+,('Vet Stephanie Mendez', 64, '1981-05-04'), ('Vet Jack Harkness', 38, '2008-06-08');
+
+select * from vets;
