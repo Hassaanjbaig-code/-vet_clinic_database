69687e313f3f68c78422a2552c6a0cd5f847353a
Micheal <oguntayomicheal@gmail.com>
2023-02-28 22:54:06 +0100

    Add join table for medical histories and treatments

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -38,9 +38,17 @@ create table invoice_items(
     foreign key (treatment_id) REFERENCES treatments(id)
 )
 
+CREATE TABLE medical_histories_has_treatments (
+    medical_history_id INT,
+    treatment_id INT,
+    FOREIGN KEY medical_history_id REFERENCES medical_histories(id),
+    FOREIGN KEY treatment_id REFERENCES treatments(id)
+);
+
+
 CREATE INDEX patient_id_asc ON medical_histories(patient_id ASC);
 CREATE INDEX medical_history_id_asc ON invoices(medical_history_id ASC);
-CREATE INDEX medical_history_id_asc ON treatment_historiy(medical_history_id ASC);
-CREATE INDEX treatment_id_asc ON treatment_historiy(treatment_id ASC);
+CREATE INDEX medical_history_id_asc ON medical_histories_has_treatments(medical_history_id ASC);
+CREATE INDEX treatment_id_asc ON medical_histories_has_treatments(treatment_id ASC);
 CREATE INDEX invoice_id_asc ON invoice_items(invoice_id ASC);
 CREATE INDEX treatment_id_asc ON invoice_items(treatment_id ASC);
